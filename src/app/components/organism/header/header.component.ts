bb2d62ade176889e5c47017ca2c30e9122e41c29
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-07-18 02:14:24 +0200

    feat(Component): :sparkles: Header
    Navigating between pages

--- src/app/components/organism/header/header.component.ts
+++ src/app/components/organism/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { routes } from 'src/app/app-routing.module';
 
 @Component({
   selector: '[app-header]',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-
+  pagesList = routes;
 }
