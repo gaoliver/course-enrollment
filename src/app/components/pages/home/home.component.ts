c1a5a84c322eed949437f5f6feb188529d28280e
Gabriel Ramos <gabriel.ramos@deptagency.com>
2023-07-20 00:37:11 +0200

    feat(Page): :sparkles: Home
    Home animation

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { User } from '@src/app/store/@types/interfaces';
+import { AppState } from '@src/app/store/app.state';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  user: User | undefined;
 
+  constructor(private store: Store<AppState>) {
+    this.store.subscribe((state) => (this.user = state.userState.user));
+  }
 }
